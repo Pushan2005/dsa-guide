dc60fb6a64875236a75bcdfd630fbcf8a1fcaf09
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2024-12-08 01:49:53 +0530

    [feature]added .md support

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 const nextConfig: NextConfig = {
-    pageExtensions: ["mdx", "ts", "tsx"],
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 
     // Note: Using the Rust compiler means we cannot use
     // rehype or remark plugins. For my app, this is fine.
